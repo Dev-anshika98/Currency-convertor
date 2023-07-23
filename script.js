975667a41afb7b8e58046444b46e8e8edf473a83
Dev-anshika98 <g.anshikacreative@gmail.com>
2023-07-23 12:28:10 +0530

    added scripts

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
 const fromDropDown = document.getElementById("from-currency-select");
 const toDropDown = document.getElementById("to-currency-select");
-
 //Create dropdown from the currencies array
 currencies.forEach((currency) => {
   const option = document.createElement("option");
@@ -9,7 +8,6 @@ currencies.forEach((currency) => {
   option.text = currency;
   fromDropDown.add(option);
 });
-
 //Repeat same thing for the other dropdown
 currencies.forEach((currency) => {
   const option = document.createElement("option");
@@ -17,17 +15,14 @@ currencies.forEach((currency) => {
   option.text = currency;
   toDropDown.add(option);
 });
-
 //Setting default values
 fromDropDown.value = "USD";
 toDropDown.value = "INR";
-
 let convertCurrency = () => {
   //Create References
   const amount = document.querySelector("#amount").value;
   const fromCurrency = fromDropDown.value;
   const toCurrency = toDropDown.value;
-
   //If amount input field is not empty
   if (amount.length != 0) {
     fetch(api)
@@ -44,7 +39,6 @@ let convertCurrency = () => {
     alert("Please fill in the amount");
   }
 };
-
 document
   .querySelector("#convert-button")
   .addEventListener("click", convertCurrency);
